8270cadfe17d1ab2143507b833358d859c838b0e
Biresh paul <palbiresh375@gmail.com>
2025-08-23 04:39:08 +0600

    payo mobile app setup

---
+++ script/index.js
@@ -0,0 +1,21 @@
+document.getElementById('loginButton').addEventListener('click',function(e){
+    e.preventDefault();
+    const inputNumber = document.getElementById('userNumber').value;
+    const convertINputNumber = parseInt(inputNumber)
+    
+    const inputPin = document.getElementById('userPin').value;
+     const convertINputPin = parseInt(inputPin);
+
+    
+    const Number = 1234567890;
+    const pin = 1234;
+    if(Number===convertINputNumber && pin===convertINputPin){
+        window.location.href= 'home.html';
+    }else{
+        alert('user or pin not valid');
+    };
+
+
+
+
+})
